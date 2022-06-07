2a85f1314cd8130af9716785aaf268bcd4a6d285
Vadym Kuzovkin <vadymkuzovkin@gmail.com>
2022-06-07 10:54:00 +0300

    Practice tasks done

--- src/routes.ts
+++ src/routes.ts
@@ -1,41 +1,56 @@
-import fs from 'fs';
-import { IncomingMessage, ServerResponse } from 'http';
+import { IncomingMessage, ServerResponse } from "http";
 
-export function requestHandler(req: IncomingMessage, res: ServerResponse) {
+
+export const requestHandler = (req: IncomingMessage, res: ServerResponse) => {
     const url = req.url;
 
     if (url === '/') {
         res.setHeader('Content-Type', 'text/html');
         res.write(`
             <html>
-                <head>
-                    <title>Page</title>
-                </head>
                 <body>
+                    <h1>Some Greeting</h1>
                     <form
-                        action="/message"
+                        action="/create-user"
                         method="POST">
-                        <input name="message" type="text">
+                        <input name="username" type="text">
                         <button>Submit</button>
                     </form>
                 </body>
             </html>
         `)
-        return res.end();
+        res.end();
+        return;
     }
 
-    if (url === '/message' && req.method === 'POST') {
-        const body: Uint8Array[] = [];
-        req.on('data', (chunk) => body.push(chunk));
-        return req.on('end', () => {
-            const parsedBody = Buffer.concat(body).toString();
-            const message = parsedBody.split('=')[1];
-
-            fs.writeFile('message.txt', message, (err) => {
-                res.writeHead(302, { Location: '/' });
-                return res.end();
-            });
-        });
+    if (url === '/users') {
+        res.setHeader('Content-Type', 'text/html');
+        res.write(`
+            <html>
+                <body>
+                    <ul>
+                        <li>User 1</li>
+                        <li>User 2</li>
+                        <li>User 3</li>
+                        <li>User 4</li>
+                    </ul>
+                </body>
+            </html>
+        `)
+        res.end();
+        return;
     }
+
+    if (url === '/create-user') {
+        const data: Uint8Array[] = [];
+        req.on('data', (chunk) => data.push(chunk))
+        req.on('end', () => {
+            const parsedData = Buffer.concat(data).toString();
+            res.writeHead(302, { Location: '/' })
+            res.end();
+            console.log(parsedData);
+        })
+        return;
+    }
+
 }
-
